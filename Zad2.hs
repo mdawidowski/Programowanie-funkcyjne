856611f86f4239e3fa2be6c1005de18763dbf224
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2018-03-04 10:31:47 +0100

    dodano zadania Haskell

---
+++ Zad2.hs
@@ -0,0 +1,6 @@
+smaller x y = x < y
+greater x y = x > y
+equal x y = x == y
+smaller_equal x y = x <= y
+greater_equal x y = x >= y
+not_equal x y = not(x == y)
---
+++ Zad8.hs
@@ -0,0 +1,1 @@
+silnia n = if n < 2 then 1 else n * silnia (n-1)
