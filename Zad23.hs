b6b7333bbe8cfd46027241571896557b07a5fd99
Marcin Dawidowski <mdawidowski@sigma.ug.edu.pl>
2019-04-27 13:10:08 +0200

    update Zad23

--- Zad23.hs
+++ Zad23.hs
@@ -1,6 +1,11 @@
 data Tree a = Node a [Tree a]
 
-tree = Node 1 [Node 2 [], Node 3 [], Node 4 []]
+tree = Node 1 [Node 5 [], Node 3 [], Node 4 []]
 
 sizeTree (Node l r) = 1 + sum(map sizeTree r)
 
+sumTree (Node a []) = a
+sumTree (Node l r) = l + sum(map sumTree r)
+
+preTree (Node a []) = [a]
+preTree (Node l r) = [l] ++ concat(map preTree r)
